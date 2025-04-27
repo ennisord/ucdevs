cee97a09eba39c6327fc9e6f86e6197b5eb1addd
ennisordenheim <124545720+ennisordenheim@users.noreply.github.com>
2025-04-27 13:50:57 -0600

    some text

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,16 @@
 import Image from "next/image";
 import Head from "next/head";
-import { Geist, Geist_Mono } from "next/font/google";
+import { League_Spartan } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const leagueSpartan = League_Spartan({
+  variable: "--font-league-spartan",
   subsets: ["latin"],
 });
 
 export default function Home() {
   return (
     <div
-      className="flex items-center justify-center min-h-screen"
+      className={`flex items-center flex-col justify-center min-h-screen ${leagueSpartan.variable}`}
       style={{ background: "#101010" }}
     >
       <Head>
@@ -30,6 +25,9 @@ export default function Home() {
         height={256}
         priority
       />
+      <p className="text-white text-center text-lg max-w-2xl" style={{ fontFamily: 'var(--font-league-spartan)' }}>
+        We&apos;re an up-and-coming club at the University of Calgary. Our primary focus will be web development, particularly working on projects for nonprofits.
+      </p>
     </div>
   );
 }
